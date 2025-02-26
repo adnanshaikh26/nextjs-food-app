f889b77a6e6c82313f23db9140bcad11f3df4d53
adnanshaikh26 <adnan.shk.26@gmail.com>
2025-02-26 16:56:00 +0530

    Added sqlite3 database

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,7 +1,26 @@
-export default function Meals() {
+import Link from "next/link";
+import classes from "./page.module.css";
+import MealsGrid from "@/components/meals/meals-grid";
+
+import { getMeals } from "@/lib/meals";
+
+export default async function Meals() {
+  const meals = await getMeals();
   return (
-    <div>
-      <h1>Meals</h1>
-    </div>
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>Choose your favorite recipe.</p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share Your Favourite Recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={meals} />
+      </main>
+    </>
   );
 }
